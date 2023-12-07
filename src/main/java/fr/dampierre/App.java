597dcf9983f88bd5bfadb13721dd21db01bdd851
Youssefsadik02 <youssefsadik2002@gmail.com>
2023-12-07 08:26:49 +0100

    modularisation de la version 1

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -6,30 +6,51 @@ import java.util.Scanner;
 public class App {
     
     public static void main(String[] args) {
-    Scanner clavier= new Scanner (System.in);
-    Random generateur = new Random();
-    System.out.println("Bienvenu dans notre jeu, Le but est de parcourir les 20 cases du jeu avec 5 lancers d'un dé. Un dé à 6 faces sera donc lancé 5 fois. On fait toujours les 5 lancers. ");
-    clavier.nextLine();
-    int i;
-    int rest=0;
-    int increment=0;
-    for ( i=1; i<=5; i++){
-         int nbreAleatoire= generateur.nextInt(6) + 1;
-         increment+=nbreAleatoire;
-         rest = 20-increment;
-         System.out.print("Lancer "+i+" : vous avez fait "+ nbreAleatoire+ ". vous etes sur la case "+increment+ " (encore "+rest +" cases)");
-         clavier.nextLine();
         
+        Scanner clavier= new Scanner (System.in);
+        System.out.println("Bienvenu dans notre jeu, Le but est de parcourir les 20 cases du jeu avec 5 lancers d'un dé.\n" + 
+        " Un dé à 6 faces sera donc lancé 5 fois. On fait toujours les 5 lancers. ");
+
+        clavier.nextLine();
+        lancers();
+        clavier.close();
+
+    }
+     // Effectuer les lancers 
+    public static void lancers (){
+        int i;
+        int rest=0;
+        int increment=0;
+        Scanner clavier= new Scanner (System.in);
+        Random generateur = new Random();
+        for ( i=1; i<=5; i++) {
+             int nbreAleatoire= generateur.nextInt(6) + 1;
+             increment+=nbreAleatoire;
+             rest = 20-increment;
+             System.out.print("Lancer "+i+" : vous avez fait "+ nbreAleatoire+ ". vous etes sur la case "+increment+ " (encore "+rest +" cases)");
+             clavier.nextLine();
+            }
+        clavier.close();
+        test(rest);
         }
-    clavier.close();
-    
-    if (rest == 0) {
-        System.out.println("Vous avez gagné!");
-    
+      
+          
+    // tester si on a gagné ou non    
+
+    public static void test(int reste){
+         if (reste == 0) {
+            System.out.println("Vous avez gagné!");
+        }
+        else {
+            System.out.println("Vous avez perdu!");
+        } 
     }
-    else {
-        System.out.println("Vous avez perdu!");
-    } 
-    }
-    
+
 }
+
+        
+        
+ 
+
+    
+
